8ce86b34c2d55e8c3ebfd921d70f714a0a3edfc1
aban-ali <22f1001086@ds.study.iitm.ac.in>
2026-04-28 14:38:15 +0000

    Naive Tiling approach, dependent on caching

---
+++ conv_cuda_base_optim.cu
@@ -0,0 +1,100 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define IMG_SIZE 512
+#define KERNEL_SIZE 3
+#define TILE_DIM 16
+
+float *img;
+__constant__ float kern[KERNEL_SIZE * KERNEL_SIZE];
+float *output;
+
+void init_img(){
+    for(int i=0; i<IMG_SIZE*IMG_SIZE; i++)
+        img[i] = rand() % 256;
+}
+void kernel_init(float *kernel){
+    for(int i=0; i<KERNEL_SIZE; i++){
+        for(int j=-1*KERNEL_SIZE/2; j<=KERNEL_SIZE/2; j++){
+            kernel[i * KERNEL_SIZE + (j + 1)] = -j;
+        }
+    }
+}
+
+__global__
+void calcTiledConvolution(float* image, float* out){
+    int row = blockIdx.y * TILE_DIM + threadIdx.y;
+    int col = blockIdx.x * TILE_DIM + threadIdx.x;
+    __shared__ float Nds[TILE_DIM][TILE_DIM];
+
+    if(row<IMG_SIZE && col<IMG_SIZE)
+        Nds[threadIdx.y][threadIdx.x] = image[ row * IMG_SIZE + col ];
+    else
+        Nds[threadIdx.y][threadIdx.x] = 0;
+    
+    __syncthreads();
+    
+    if(row<IMG_SIZE && col<IMG_SIZE){
+            float val = 0;
+        for(int i=-KERNEL_SIZE/2; i<=KERNEL_SIZE/2; i++){
+            for(int j=-KERNEL_SIZE/2; j<=KERNEL_SIZE/2; j++){
+                int krow = threadIdx.y + i;
+                int kcol = threadIdx.x + j;
+
+                if(krow>=0 && krow<TILE_DIM && kcol>=0 && kcol<TILE_DIM)
+                    val += Nds[krow][kcol] * kern[ (i+KERNEL_SIZE/2) * KERNEL_SIZE + (j+KERNEL_SIZE/2) ];
+                else if(row+i>=0 && row+i<IMG_SIZE && col+j>=0 && col+j<IMG_SIZE)
+                    val += kern[(i+KERNEL_SIZE/2) * KERNEL_SIZE + (j+KERNEL_SIZE/2)] * 
+                            image[ ( row + i )*IMG_SIZE + ( col + j ) ];  // will be cached in, perhaps, L2 memory
+            }
+        }
+        out[row * IMG_SIZE + col] = val;
+    }
+}
+
+void convolution(float* A_h, float* kern_h, float* O_h){
+    float *A_d, *O_d;
+    int size_img = IMG_SIZE * IMG_SIZE * sizeof(float);
+    int size_ker = KERNEL_SIZE * KERNEL_SIZE * sizeof(float);
+    int size_out = IMG_SIZE * IMG_SIZE * sizeof(float);
+    
+    cudaMalloc((void**)&A_d, size_img);
+    cudaMalloc((void**)&O_d, size_out);
+
+    cudaMemcpy(A_d, A_h, size_img, cudaMemcpyHostToDevice);
+    cudaMemcpyToSymbol(kern, kern_h, size_ker);   // copies the data to GPUs constant memory.
+
+    dim3 threads(TILE_DIM, TILE_DIM);
+    dim3 blocks( ( IMG_SIZE + TILE_DIM - 1 )/TILE_DIM, ( IMG_SIZE + TILE_DIM - 1 )/TILE_DIM );   
+    calcTiledConvolution<<<blocks, threads>>>(A_d, O_d);
+
+    cudaError_t err = cudaGetLastError();
+    if(err != cudaSuccess){
+        printf("CUDA Launch Error : \t%s", cudaGetErrorString(err));
+    }
+    cudaError_t err1 = cudaDeviceSynchronize();
+    if(err1 != cudaSuccess){
+        printf("CUDA Execution Error : \t%s", cudaGetErrorString(err1));
+    }
+
+    cudaMemcpy(O_h, O_d, size_out, cudaMemcpyDeviceToHost);
+
+    cudaFree(A_d);
+    cudaFree(O_d);
+}
+
+int main(){
+    float *kernel;
+    img     = (float*)malloc(IMG_SIZE * IMG_SIZE * sizeof(float));
+    kernel  = (float*)malloc(KERNEL_SIZE * KERNEL_SIZE * sizeof(float));
+    output  = (float*)malloc(IMG_SIZE * IMG_SIZE * sizeof(float));
+    
+    init_img();
+    kernel_init(kernel);
+    convolution(img, kernel, output);
+
+    free(img);
+    free(output);
+
+    return 0;
+}
